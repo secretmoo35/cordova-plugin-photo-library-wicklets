f0aee8ac66d26829cb6f6482a481b239568796cb
Luca Damasco <luca.damasco@gmail.com>
2020-08-14 10:09:53 -0400

    Update plugin to use newer Swift version for image data.

--- src/ios/PhotoLibraryService.swift
+++ src/ios/PhotoLibraryService.swift
@@ -584,7 +584,7 @@ final class PhotoLibraryService {
         }
 
         // Permission was manually denied by user, open settings screen
-        let settingsUrl = URL(string: UIApplicationOpenSettingsURLString)
+        let settingsUrl = URL(string: UIApplication.openSettingsURLString)
         if let url = settingsUrl {
             UIApplication.shared.openURL(url)
             // TODO: run callback only when return ?
@@ -830,10 +830,10 @@ func saveVideo(_ url: String, album: String, completion: @escaping (_ assetId: S
         var mimeType: String?
 
         if (imageHasAlpha(image)){
-            data = UIImagePNGRepresentation(image)
+            data = image.pngData()
             mimeType = data != nil ? "image/png" : nil
         } else {
-            data = UIImageJPEGRepresentation(image, CGFloat(quality))
+            data = image.jpegData(compressionQuality: CGFloat(quality))
             mimeType = data != nil ? "image/jpeg" : nil
         }
 
